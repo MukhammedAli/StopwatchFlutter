0204cdd9dd55cb96672da8b656585857bf48ac0a
MukhammedAli <190103412@stu.sdu.edu.kz>
2022-07-06 15:57:31 +0600

    Code refactoring adding laps

--- lib/stopwatch.dart
+++ lib/stopwatch.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import './stopwatch.dart';
 
@@ -11,9 +12,11 @@ class _StopWatchState extends State<StopWatch> {
   bool isTicking = false;
   int seconds = 0;
   late Timer timer;
+  final laps = <int>[];
 
   void _startTimer() {
-    timer = Timer.periodic(Duration(seconds: 1), _onTick);
+    laps.clear();
+    timer = Timer.periodic(Duration(milliseconds: 100), _onTick);
 
     setState(() {
       isTicking = true;
@@ -36,10 +39,76 @@ class _StopWatchState extends State<StopWatch> {
   //   timer = Timer.periodic(Duration(seconds: 1), _onTick);
   // }
 
+  int milliseconds = 0;
+
   void _onTick(Timer time) {
     setState(() {
-      ++seconds;
+      milliseconds += 100;
     });
+  }
+
+  void _lap() {
+    setState(() {
+      laps.add(milliseconds);
+      milliseconds = 0;
+    });
+  }
+
+  Widget _buildControls(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+        ),
+        child: Text('Start'),
+        onPressed: isTicking ? null : _startTimer,
+      ),
+      SizedBox(width: 20),
+      ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
+        ),
+        child: Text('Lap'),
+        onPressed: isTicking ? _lap : null,
+      ),
+      SizedBox(width: 20),
+      TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+        ),
+        child: Text('Stop'),
+        onPressed: isTicking ? _stopTimer : null,
+      )
+    ]);
+  }
+
+  Widget _buildCounter(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Lap ${laps.length + 1}',
+            style: Theme.of(context)
+                .textTheme
+                .subtitle1
+                ?.copyWith(color: Colors.white),
+          ),
+          Text(
+            _secondsText(milliseconds),
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                ?.copyWith(color: Colors.white),
+          ),
+          SizedBox(height: 20),
+          _buildControls(context),
+        ],
+      ),
+    );
   }
 
   @override
@@ -48,41 +117,13 @@ class _StopWatchState extends State<StopWatch> {
         appBar: AppBar(
           title: Text('Stopwatch'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '$seconds ${_secondsText()}',
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            SizedBox(height: 20),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.green),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                ),
-                child: Text('Start'),
-                onPressed: isTicking ? null : _startTimer,
-              ),
-              SizedBox(width: 20),
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                ),
-                child: Text('Stop'),
-                onPressed: isTicking ? _stopTimer : null,
-              )
-            ]),
-          ],
-        ));
+        body: _buildCounter(context));
   }
 
-  String _secondsText() => seconds == 1 ? 'second' : 'seconds';
+  String _secondsText(int milliseconds) {
+    final seconds = milliseconds / 1000;
+    return '$seconds seconds';
+  }
 
   @override
   void dispose() {
